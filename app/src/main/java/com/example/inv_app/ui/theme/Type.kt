4441c8309c25677f7912de9d59a57178c90d2b8d
jssdvd <jssdvv@gmail.com>
2023-08-05 00:57:00 -0500

    Created variable to manage FontFamily
    Took 33 seconds

--- app/src/main/java/com/example/inv_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/inv_app/ui/theme/Type.kt
@@ -43,108 +43,110 @@ val Hum521BTFontFamily = FontFamily(
     )
 )
 
+val fontFamily = FontFamily.Default
+
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = 0.sp, //Must To Be Adjusted To -0.25 Defined By Material 3 Documentation
     ),
     displayMedium = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = Hum521BTFontFamily,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Hum521BTFontFamily,
-        fontWeight = FontWeight.Bold,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
