250447ae4dd813f4c24a4e45f3ebecea05db83a3
jssdvd <jssdvv@gmail.com>
2023-08-21 17:38:02 -0500

    Deleted files from their directory and replaced with new in other directories
    Took 2 minutes

--- app/src/main/java/com/example/inv_app/presentation/features/scanner/ScannerHistoryScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.inv_app.presentation.features.scanner
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun ScannerHistoryScreen() {
-    Text(text = "Historial")
-}
